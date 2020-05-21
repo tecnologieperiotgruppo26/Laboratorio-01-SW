b262a82f6d0c094f8bc82bfcd34eee7916d7fd95
Simone <simone.alberto@icloud.com>
2020-05-21 16:29:43 +0200

    simo

---
+++ 02_04_Laboratorio_IoT_SW/app.py
@@ -0,0 +1,25 @@
+import os, os.path 
+import random 
+import string 
+import cherrypy
+
+class StringGenerator(object): 
+  @cherrypy.expose
+
+  def index(self):
+    return open('./freeboard/index.html')
+
+if __name__ == '__main__': 
+  conf = {
+  '/': {
+    'tools.sessions.on': True,
+    'tools.staticdir.root': os.path.abspath(os.getcwd()) 
+  },
+  '/freeboard': {
+    'tools.staticdir.on': True,
+    'tools.staticdir.dir': './freeboard' 
+    }
+  }
+  cherrypy.tree.mount(StringGenerator(), '/', conf) 
+  cherrypy.engine.start()
+  cherrypy.engine.block()
