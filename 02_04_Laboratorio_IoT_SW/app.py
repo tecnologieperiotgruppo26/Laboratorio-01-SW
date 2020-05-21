00907ef789418bd835aebb920196acbbda1cffdd
Lucabalduzzi <baldovino.1998@gmail.com>
2020-05-21 15:58:42 +0200

    Es 03 finito

--- 02_04_Laboratorio_IoT_SW/app.py
+++ 02_04_Laboratorio_IoT_SW/app.py
@@ -1,9 +1,40 @@
 import cherrypy
+import json
 
-class StringGeneratorWebService(object):
+class Converter():
     cherrypy.exposed = True
-    def GET (self, *uri, ** params):
-        return ("URI: %s; Parameters %s" % (str (uri), str(params)))
+    
+    def PUT(self, *uri):
+        str_json = cherrypy.request.body.read()
+        input_dict = json.load(str_json)
+        output_dict = {
+            "originalValues": input_dict["values"]
+            "convertedValues": []
+        }
+        for num in output_dict["originalValue"]:
+            value = convert(num, input_dict["originalUnit"], input_dict["targetUnit"])
+            output_dict["convertedValues"].append(value)
+        return json.dump(output_dict)
+    
+    def convert(num: float, originalUnit: str, targetUnit: str) -> float:
+        finalValue = 0
+        if "originalUnit" == 'C':
+            if "targetUnit" == 'K':
+                finalValue = float(num) + 273.15
+            elif "targetUnit" == 'F':
+                finalValue = (float(num)*9/5)+32
+        elif "originalUnit" == 'K':
+            if "targetUnit" == 'C':
+                finalValue = float(num) - 273.15
+            elif "targetUnit" == 'F':
+                finalValue = (float(num-273.15)*9/5)+32
+        elif "originalUnit" == 'F':
+            if "targetUnit" == 'C':
+                finalValue = ((float(num) - 32) * 5 / 9)
+            elif "targetUnit" == 'K':
+                #(5 °F - 32) × 5/9 + 273,15
+                finalValue = ((float(num) - 32) * 5 / 9) + 273.15
+    return finalValue 
     
 if __name__ == '__main__':
     conf = {
@@ -12,8 +43,7 @@ if __name__ == '__main__':
             'tools.sessions.on': True,
         }
     }
-    cherrypy.tree.mount (StringGeneratorWebService(), '/string', conf)
-    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    cherrypy.tree.mount (Converter(), '/string', conf)
     cherrypy.config.update({'server.socket_port': 8080})
     
     cherrypy.engine.start()
