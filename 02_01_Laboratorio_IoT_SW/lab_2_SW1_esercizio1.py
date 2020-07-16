3cc5f0eeb01135ba68ae82807e87484219a75a90
Simone <simone.alberto@icloud.com>
2020-07-16 22:58:14 +0200

    fix

--- 02_01_Laboratorio_IoT_SW/lab_2_SW1_esercizio1.py
+++ 02_01_Laboratorio_IoT_SW/lab_2_SW1_esercizio1.py
@@ -1,7 +1,7 @@
 import cherrypy
 import json
 
-class HelloWorld2(object):
+class WebServer(object):
     #@cherrypy.expose
     exposed = True
 
@@ -12,17 +12,25 @@ class HelloWorld2(object):
                 finalValue = float(params["value"]) + 273.15
             elif params ["targetUnit"] == 'F':
                 finalValue = (float(params["value"])*9/5)+32
+            else:
+                raise cherrypy.HTTPError(404, 'Si è utilizzato un comando non valido!')
         elif params["originalUnit"] == 'K':
             if params["targetUnit"] == 'C':
                 finalValue = float(params["value"]) - 273.15
             elif params ["targetUnit"] == 'F':
                 finalValue = (float(params["value"]-273.15)*9/5)+32
+            else:
+                raise cherrypy.HTTPError(404, 'Si è utilizzato un comando non valido!')
         elif params["originalUnit"] == 'F':
             if params["targetUnit"] == 'C':
                 finalValue = ((float(params["value"]) - 32) * 5 / 9)
             elif params["targetUnit"] == 'K':
                 #(5 °F - 32) × 5/9 + 273,15
                 finalValue = ((float(params["value"]) - 32) * 5 / 9) + 273.15
+            else:
+                raise cherrypy.HTTPError(404, 'Si è utilizzato un comando non valido!')
+        else:
+            raise cherrypy.HTTPError(404, 'Si è utilizzato un comando non valido!')
 
         dict = {"value": "{}".format(round(finalValue, 2)),
                 "originalUnit": str(params["originalUnit"]),
@@ -40,13 +48,9 @@ if __name__ == "__main__":
         }
     }
 
-    cherrypy.tree.mount(HelloWorld2(), '/converter', conf)
+    cherrypy.tree.mount(WebServer(), '/converter', conf)
     cherrypy.config.update({'server.socket_host': '127.0.0.1'})
     cherrypy.config.update({'server.socket_port': 8080})
     cherrypy.engine.start()
     input()
     cherrypy.engine.stop()
-
-    #    cherrypy.quickstart(HelloWorld2(),'/',conf)
-
-
